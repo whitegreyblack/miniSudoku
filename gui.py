0a7bdbcb34685c12dbbbbe495db74cc98c789bab
Sam Whang <sangwoowhang@gmail.com>
2017-07-27 15:05:29 -0500

    Finished creating basic outline for sudoku

--- gui.py
+++ gui.py
@@ -1,58 +1,106 @@
 import sudoku
 import curses
+import curses.panel
 
 # 37 x 21
 chars=[]
-def printbottom(board, index, c):
-    board.addstr(index, c, sudoku.box)
-    board.addstr(index+1, c, sudoku.div)
-    return index+2
-def printcenter(board, index, c):
-    for i in range(2):
-        board.addstr(index, c, sudoku.box)
-        board.addstr(index+1, c, sudoku.top)
-        index += 2
-    return index
-def printlines(board, c):
-    index = 2
-    board.addstr(index,c,sudoku.bot)
-    index += 1
-    for i in range(2):
-        index=printcenter(board, index, c)
-        index=printbottom(board, index, c)
-    index=printcenter(board, index, c)
-    board.addstr(index, c, sudoku.box)
-    board.addstr(index+1, c, sudoku.bot)
-def letterstring():
-    string=" ".join(list("  A B C D E F G H I"))
-    return string
-def makeboard():
-    board=curses.newwin(22,41,0,0)
-    board.border()
-    board.addstr(1,1,letterstring())
-    index=3
-    number=1
-    for i in range(9):
-        board.addstr(index, 1, "{}".format(number))
-        number+=1
-        index+=2
-    col = 3
-    printlines(board, col)
-    return board
+
 def setup():
     curses.curs_set(0)
-    chars.append(curses.ACS_BLOCK)
-    chars.append(curses.ACS_BSSB)
+    chars.append(curses.ACS_BLOCK)                         # BLOCK
+    chars.append(curses.ACS_BSSB)                          # UPPER RIGHT
+    chars.append(curses.ACS_BSBS)                          # Horizontal Line
+    chars.append(curses.ACS_BBSS)                          # UPPER LEFT
+    chars.append(curses.ACS_TTEE)                          # TOP TEE
+    chars.append(curses.ACS_BTEE)                          # BOT TEE
+    chars.append(curses.ACS_VLINE)                         # SCAN LINE
+    chars.append(curses.ACS_PLUS)                          # PLUS
+    chars.append(curses.ACS_LTEE)                          # LEFT TEE
+    chars.append(curses.ACS_RTEE)                          # RIGHT TEE
+
+def makepuzzle():
+    puzzle = sudoku.Grid()
+    puzzle.fill()
+    return puzzle
+
+def fillgrid(g, p):
+    for j in range(9):
+        for i in range(9):
+            g.addch(i*2+1, j*4+2, "{}".format(p.grid[i][j]))
+
+def makegrid(l=19, c=37):
+    '''
+    creates the grid lines
+    '''
+    def row(i, char, outer, inner, step):                  # print magic
+        start = step                                       # double for loop
+        for m in range(outer):
+            for n in range(inner):
+                g.addch(i, start, char)
+                start+=step
+            start+=step
+        return i+1
+
+    def midrow(i=0):
+        row(i, chars[4], 1, 2, 12)                         # outer down tees
+
+        for i in range(1, l-1):
+
+            if i % 2:                                      # inner grid
+                row(i, '|', 3, 2, 4)                       # vertical bars 
+
+            else:
+                row(i, '-', 3, 11, 1)                      # horizontal bars
+                row(i, ' ', 3, 2, 4)                       # pluses
+
+        for i in range(1, l-1):                            # outer grid
+            row(i, chars[6], 1, 2, 12)                     # vertical bars
+            if i % 6 == 0:
+                row(i, chars[2], 1, c-2, 1)                # horizontal lines
+                row(i, chars[7], 1, 2, 12)                 # pluses
+                row(i, chars[8], 1, 1, 0)                  # left tees
+                g.addch(i, c-1, chars[9])                  # right tees
+        row(i+1, chars[5], 1, 2, 12)                       # up tees
+
+    g = curses.newwin(19, 37, 2, 3)
+    g.border()
+    index = midrow()
+    return g
+
+def makeboard(l=22, c=41):
+
+    def addlabels(board):
+
+        def letterstring():                                # top label
+            return " ".join(list("  A B C D E F G H I"))
+
+        board.addstr(1, 1, letterstring())                 # A-Z
+
+        index, number = 3, 1
+        for i in range(9):
+            board.addstr(index, 2, "{}".format(number))    # 1-9
+            number += 1
+            index += 2
+
+    board=curses.newwin(l,c,0,0)
+    board.border()
+    addlabels(board)                                       # grid labels
+    return board
+
+def play(b):
+    b.getch()
 
 def main(scr):
-    scr.addstr(1,1,"{}".format(scr.getmaxyx()))
-    
-    # adds ACS characterset to build board
-    setup()
-    scr.addstr(2, 1,"making border")
-    board = makeboard() 
-    char = board.getch() 
 
+    puzzle = makepuzzle()                                  # Sudoku puzzle
+
+    setup()
+    board = makeboard()                                    # outer box
+    grid = makegrid()                                      # inner box
+    fillgrid(grid, puzzle)
+    grid.overwrite(board)                                  # puts grid on top
+
+    play(board)                                            # Solve it if you can!
 
 if __name__ == "__main__":
     curses.wrapper(main)
--- sudoku.py
+++ sudoku.py
@@ -1,16 +1,42 @@
 # Sudoku app using curses to create the board
+import pprint
 
 __author__ = "Sam Whang | WGB"
 
-top="#---+---+---#---+---+---#---+---+---#"
-div="############@###########@############"
-box="#   |   |   #   |   |   #   |   |   #"
-bot="#####################################"
-ovr="+---+---+---+---+---+---+---+---+---+"
-num="| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |"
-und="+---+---+---+---+---+---+---+---+---+"
+class Cell:
+    def __init__(self, row, col, box):
+        self.pos = (row, col, box)
+        self.val = None
+    def empty():
+        return self.val is 0 or self.val is None
+
+class Grid:
+    def __init__(self):
+        self.grid = [[0 for _ in range(9)] for _ in range(9)]
+        self.grid=[[3,0,6,5,0,8,4,0,0],
+		   [5,2,0,0,0,0,0,0,0],
+		   [0,8,7,0,0,0,0,3,1],
+		   [0,0,3,0,1,0,0,8,0],
+		   [9,0,0,8,6,3,0,0,5],
+		   [0,5,0,0,9,0,6,0,0],
+		   [1,3,0,0,0,0,2,5,0],
+		   [0,0,0,0,0,0,0,7,4],
+		   [0,0,5,2,0,6,3,0,0]] 
+    def fill(self):
+        pass
+    def draw(self):
+        pprint.pprint(self.grid)
+    def solve(self):
+        pass 
 
 if __name__ == "__main__":
+    top="#---+---+---#---+---+---#---+---+---#"
+    div="############@###########@############"
+    box="#   |   |   #   |   |   #   |   |   #"
+    bot="#####################################"
+    ovr="+---+---+---+---+---+---+---+---+---+"
+    num="| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |"
+    und="+---+---+---+---+---+---+---+---+---+"
     print(bot)
     print(box)
     print(top)
@@ -35,3 +61,6 @@ if __name__ == "__main__":
     print(top)
     print(len(top))
     print(33-12)
+
+    grid = Grid()
+    grid.draw()
