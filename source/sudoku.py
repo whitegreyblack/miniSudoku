e8c84dcb5d7dbdcdd4b93f0e42374fe628fa59b3
Sam Whang <sangwoowhang@gmail.com>
2018-12-19 15:18:18 -0600

    Created a tiny sudoku board

--- source/sudoku.py
+++ source/sudoku.py
@@ -90,8 +90,99 @@ class Block:
         }
 
 class UIGrid(ListGrid):
+    def __init__(self, grid: list=None, build: bool=True) -> None:
+        super().__init__(grid, build)
+        self.build_border_tiny()
+        self.build_border_small()
+        self.build_border_large()
+
+    def build_border_tiny(self):
+        self.border_tiny = [[' ' for _ in range(80)] for _ in range(25)]
+        self.border_tiny[0][0] = HEAVY_TLC
+        self.border_tiny[0][24] = HEAVY_TRC
+        self.border_tiny[12][0] = HEAVY_BLC
+        self.border_tiny[12][24] = HEAVY_BRC
+        # vertical bars
+        for y in range(1, 12):
+            for x in (0, 24):
+                self.border_tiny[y][x] = HEAVY_VBAR
+            for x in (8, 16):
+                self.border_tiny[y][x] = LIGHT_VBAR
+        for x in range(1, 24):
+            for y in (0, 12):
+                self.border_tiny[y][x] = HEAVY_HBAR
+            for y in (4, 8):
+                self.border_tiny[y][x] = LIGHT_HBAR
+
+        for y in (4, 8):
+            for x in (8, 16):
+                self.border_tiny[y][x] = LIGHT_CRS
+
+    def build_border_small(self):
+        self.border_small = [[' ' for _ in range(80)] for _ in range(25)]
+        self.border_small[0][0] = HEAVY_TLC
+        self.border_small[0][44] = HEAVY_TRC
+        self.border_small[24][0] = HEAVY_BLC
+        self.border_small[24][44] = HEAVY_BRC
+        
+        for y in range(1, 23):
+            for x in range(14, 44, 14):
+                if x % 14 == 0:
+                    self.border_small[y][x] = HEAVY_VBAR
+                else:
+                    self.border_small[y][x] = LIGHT_VBAR
+
+        # horizontal bars
+        for y in range(0, 25, 8):
+            heavy = y % 6 == 0
+            for x in range(1, 44):
+                self.border_small[y][x] = HEAVY_HBAR if heavy else LIGHT_HBAR
+
+    def build_border_large(self):
+        self.border_large = [[' ' for _ in range(73)] for _ in range(37)]
+        self.border_large[0][0] = HEAVY_TLC
+        self.border_large[0][72] = HEAVY_TRC
+        self.border_large[36][0] = HEAVY_BLC
+        self.border_large[36][72] = HEAVY_BRC
+
     @property
-    def terminal(self) -> str:
+    def cursor(self):
+        yield 1, 1, "[ ]"
+
+    @property
+    def values_tiny_board(self) -> str:
+        for by in range(3):
+            for bx in range(3):
+                for y in range(3):
+                    for x in range(3):
+                        val = self.grid[by*27+bx*3+y*9+x] # access by grid index
+                        if val != 0:
+                            yield bx*8+x*2+2, by*4+y*1+1, str(val) # access by row, col, block
+
+    @property
+    def values_small_board(self) -> str:
+        for by in range(3):
+            for bx in range(3):
+                for y in range(3):
+                    for x in range(3):
+                        val = self.grid[by*27+bx*3+y*9+x] # access by grid index
+                        if val != 0:
+                            yield bx*8+x*2+1, by*4+y*1+1, str(val) # access by row, col, block
+    
+    @property
+    def border_tiny_board(self) -> str:
+        for y, row in enumerate(self.border_tiny):
+            for x, val in enumerate(row):
+                yield x, y, val
+    
+    @property
+    def border_small_board(self) -> str:
+        for y, row in enumerate(self.border_small):
+            for x, val in enumerate(row):
+                yield x, y, val
+
+    @property
+    def values(self) -> str:
         for by in range(3):
             for bx in range(3):
                 for y in range(3):
@@ -105,6 +196,61 @@ class UIGrid(ListGrid):
                                 else:
                                     rows.append(f" " * 7)
                             yield bx*24+x*8+1, by*12+y*4+1, "\n".join(rows)
+    @property
+    def border(self) -> str:
+        g = [[' ' for _ in range(73)] for _ in range(37)]
+        g[0][0] = HEAVY_TLC
+        g[0][72] = HEAVY_TRC
+        g[36][0] = HEAVY_BLC
+        g[36][72] = HEAVY_BRC
+
+        # goes down from 0 -> y
+        # for y in range(1, 36):
+        #     for x in range(8, 64, 8):
+        #         print(y, x)
+        #         g[y][x] = HEAVY_VBAR if x % 24 == 0 else LIGHT_VBAR
+        #     # g[y][0] = HEAVY_VBAR
+        #     # g[y][8] = LIGHT_VBAR
+        #     # g[y][16] = LIGHT_VBAR
+        #     # g[y][24] = HEAVY_VBAR
+        #     # g[y][32] = LIGHT_VBAR
+        #     # g[y][40] = LIGHT_VBAR
+        #     # g[y][48] = HEAVY_VBAR
+        #     # g[y][56] = LIGHT_VBAR
+        #     # g[y][64] = LIGHT_VBAR
+        #     # g[y][72] = HEAVY_VBAR
+        #     if y % 12 == 0:
+        #         g[y][0] = HEAVY_ABR
+        #         g[y][72] = HEAVY_EBR
+        return ''.join(''.join(r) for r in g)
+
+        # # horizontal bars
+        # for x in range(0, 37, 4):
+        #     symbols = LIGHT_HBAR * 71
+        #     if x % 12 == 0:
+        #         symbols = HEAVY_HBAR * 71
+        #     t.puts(1, x, symbols)
+
+        # for x in (24, 48):
+        #     for y in range(4, 33, 4):
+        #         t.puts(x, y, HEAVY_VBAR)
+
+        # for x in range(8, 74, 8):
+        #     if x % 24 != 0:
+        #         for y in range(4, 33, 4):
+        #             if y % 12 != 0:
+        #                 t.puts(x, y, LIGHT_CRS)
+
+        # for x in (24, 48):
+        #     for y in (12, 24):
+        #         t.puts(x, y, HEAVY_CRS)
+
+        # # t.puts(8, 0, HLIGHT_TBR)
+        # t.puts(24, 0, HEAVY_TBR)
+        # t.puts(48, 0, HEAVY_TBR)
+        # t.puts(24, 36, "\u2538")
+        # t.puts(48, 36, "\u2538")
+
     @property
     def index(self):
         return self._i
@@ -161,100 +307,20 @@ if __name__ == "__main__":
 
     ui = UIGrid.init_incomplete(build=False)
     print(ui)
-    # l = ListGrid.init_incomplete()
-    # print(l)
-    # m = MatrixGrid.init_incomplete()
-    # print(m)
-    # print(l == m)
     escape_codes = [t.TK_Q, t.TK_ESCAPE, 224]
 
     t.open()
-    t.set("window.size=73x37, window.title='Sudoku'")
-    # for i, j, s in list(ui.terminal):
-    #     t.puts(i, j, s)
-    # for index, string in enumerate(list(ui.board)):
-    #     i, j = index // 3, index % 3
-    #     t.puts(i + i * 6, 0 + 8 * j, string)
-    # t.puts(1, 0, ui.board)
-    # t.puts(24, 0, ui.board)
-    # t.puts(48, 0, ui.board)
-    # t.puts(0, 12, ui.board)
-    # t.puts(24, 12, ui.board)
-    # t.puts(48, 12, ui.board)
-    # t.puts(0, 24, ui.board)
-    # t.puts(24, 24, ui.board)
-    # t.puts(48, 24, ui.board)
+    # t.set("window.size=73x37, window.title='Sudoku'")
 
-    # for i in range(0, 74, 8):
-    #     print(i)
-
-    ystepbig = 24
-    ystepsmall = 8
-
-    for y in range(0, 36):
-        for x in range(0, 74):
-            ymod = y % 4 == 0
-            xmod = x % 8 == 0
-            if xmod and ymod:
-                pass
-            elif xmod:
-                pass
-            elif ymod:
-                pass
-            else:
-                pass
-
-    t.puts(0, 0, HEAVY_TLC)
-    t.puts(72, 0, HEAVY_TRC)
-
-    t.puts(0, 36, HEAVY_BLC)
-    t.puts(72, 36, HEAVY_BRC)
-
-    # goes down from 0 -> y
-    for y in range(1, 36):
-        t.puts(0, y, HEAVY_VBAR)
-        t.puts(8, y, LIGHT_VBAR)
-        t.puts(16, y, LIGHT_VBAR)
-        t.puts(24, y, HEAVY_VBAR)
-        t.puts(32, y, LIGHT_VBAR)
-        t.puts(40, y, LIGHT_VBAR)
-        t.puts(48, y, HEAVY_VBAR)
-        t.puts(56, y, LIGHT_VBAR)
-        t.puts(64, y, LIGHT_VBAR)
-        t.puts(72, y, HEAVY_VBAR)
-        if y % 12 == 0:
-            t.puts(0, y, HEAVY_ABR)
-            t.puts(72, y, HEAVY_EBR)
-
-    # horizontal bars
-    for x in range(0, 37, 4):
-        symbols = LIGHT_HBAR * 71
-        if x % 12 == 0:
-            symbols = HEAVY_HBAR * 71
-        t.puts(1, x, symbols)
-
-    for x in (24, 48):
-        for y in range(4, 33, 4):
-            t.puts(x, y, HEAVY_VBAR)
-
-    for x in range(8, 74, 8):
-        if x % 24 != 0:
-            for y in range(4, 33, 4):
-                if y % 12 != 0:
-                    t.puts(x, y, LIGHT_CRS)
-
-    for x in (24, 48):
-        for y in (12, 24):
-            t.puts(x, y, HEAVY_CRS)
-
-    # t.puts(8, 0, HLIGHT_TBR)
-    t.puts(24, 0, HEAVY_TBR)
-    t.puts(48, 0, HEAVY_TBR)
-    t.puts(24, 36, "\u2538")
-    t.puts(48, 36, "\u2538")
-
-    for i, j, s in list(ui.terminal):
+    for i, j, s in list(ui.border_tiny_board):
         t.puts(i, j, s)
+    
+    print(*list(ui.cursor))
+
+    for i, j, s in list(ui.values_tiny_board):
+        t.puts(i, j, s)
+
+    t.puts(*list(ui.cursor)[0])
 
     t.refresh()
     c = t.read()
